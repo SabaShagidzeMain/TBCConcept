6ec7f58cf81bd712b9ed4891ce76bdef479de9e7
SabaShagidze59 <saba.webdev59@gmail.com>
2024-07-24 00:14:35 +0400

    Worked on body

--- javascript/script.js
+++ javascript/script.js
@@ -13,31 +13,39 @@ hamburgerEl.addEventListener("click", () => {
 // });
 
 // Navigation - Dropdown
-dropOne = document.getElementById("dropdown1");
-dropTwo = document.getElementById("dropdown2");
-dropThree = document.getElementById("dropdown3");
-dropDiv = document.getElementById("dropDownDiv");
+const dropOne = document.getElementById("dropdown1");
+const dropTwo = document.getElementById("dropdown2");
+const dropThree = document.getElementById("dropdown3");
+const dropDivOne = document.getElementById("dropDownDiv1");
+const dropDivTwo = document.getElementById("dropDownDiv2");
+const dropDivThree = document.getElementById("dropDownDiv3");
 
 function dropdown1() {
   dropOne.classList.toggle("show");
   dropTwo.classList.remove("show");
   dropThree.classList.remove("show");
 
-  dropDiv.classList.add("header-dropdown-show");
+  dropDivOne.classList.toggle("header-dropdown-show");
+  dropDivTwo.classList.remove("header-dropdown-show");
+  dropDivThree.classList.remove("header-dropdown-show");
 }
 function dropdown2() {
   dropTwo.classList.toggle("show");
   dropOne.classList.remove("show");
   dropThree.classList.remove("show");
 
-  dropDiv.classList.add("header-dropdown-show");
+  dropDivOne.classList.remove("header-dropdown-show");
+  dropDivTwo.classList.toggle("header-dropdown-show");
+  dropDivThree.classList.remove("header-dropdown-show");
 }
 function dropdown3() {
   dropThree.classList.toggle("show");
   dropOne.classList.remove("show");
   dropTwo.classList.remove("show");
 
-  dropDiv.classList.add("header-dropdown-show");
+  dropDivOne.classList.remove("header-dropdown-show");
+  dropDivTwo.classList.remove("header-dropdown-show");
+  dropDivThree.classList.toggle("header-dropdown-show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
@@ -49,7 +57,9 @@ window.onclick = function (event) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains("show")) {
         openDropdown.classList.remove("show");
-        dropDiv.classList.remove("header-dropdown-show");
+        dropDivOne.classList.remove("header-dropdown-show");
+        dropDivTwo.classList.remove("header-dropdown-show");
+        dropDivThree.classList.remove("header-dropdown-show");
       }
     }
   }
